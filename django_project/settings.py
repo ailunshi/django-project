cd074cb696c461c4ef40e8a914e0cde0d1bb7177
Ailun Shi <ailunshi@berkeley.edu>
2025-02-11 12:30:49 -0800

    Fix database

--- django_project/settings.py
+++ django_project/settings.py
@@ -83,16 +83,16 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "d9os2qqeksk3se",
-#         "USER": "u82hjvqa0810or",
-#         "PASSWORD": "p616014763dfe0972a7229edf92020ef24381167018d6eff9340913cf60f044d9",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "d9os2qqeksk3se",
+        "USER": "u82hjvqa0810or",
+        "PASSWORD": "p616014763dfe0972a7229edf92020ef24381167018d6eff9340913cf60f044d9",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 # os.environ['DATABASE_URL']
